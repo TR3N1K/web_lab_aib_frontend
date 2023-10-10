bc0949d7a073039a622b6da48fa01c97edd0b890
TR3N1K <deeesg@yandex.ru>
2023-10-10 15:36:53 +0300

    Task 2 Lab 8 Update

--- labs/Lab_08_events_js/Solution/Task2/app.js
+++ labs/Lab_08_events_js/Solution/Task2/app.js
@@ -1,3 +1,15 @@
 function changeBackground(color) {
     document.body.style.background = color;
 }
+function generateColor() {
+    var red = parseInt(document.getElementById("red").value);
+    var green = parseInt(document.getElementById("green").value);
+    var blue = parseInt(document.getElementById("blue").value);
+    
+    if (isNaN(red) || red < 0 || red > 255 || isNaN(green) || green < 0 || green > 255 || isNaN(blue) || blue < 0 || blue > 255) {
+      alert("Please enter valid RGB values between 0 and 255.");
+      return;
+    }
+    var color = "rgb(" + red + ", " + green + ", " + blue + ")";
+    document.getElementById("colorArea").style.backgroundColor = color;
+}
