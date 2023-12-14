feaadc19be58343e58d0e2d797dd4fdec70ddd8a
TR3N1K <DEEESg@yandex.ru>
2023-12-14 10:29:39 +0300

    Lab 13 was uploaded!

--- labs/Lab_13_python_oop/Solutions/main.py
+++ labs/Lab_13_python_oop/Solutions/main.py
@@ -1,10 +1,23 @@
-import datetime
-import openpyxl
-from block0 import create_excel_file
+from datetime import datetime
+import json
+import os
+from writer import XlsAnalyticPaymentWriter
 
-# Определяем период выгрузки
-start_date = datetime.datetime(2023, 1, 1)
-end_date = datetime.datetime.now()
+def load_data(file_path):
+    full_path = os.path.join(
+        ".\Lab_13_python_oop",
+        file_path
+    )
+    with open(full_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
 
-# Создаем файл Test2.xlsx с меткой времени создания и периодом выгрузки
-create_excel_file('Test2.xlsx', datetime.datetime.now(), start_date, end_date)
+if __name__ == '__main__':
+    data_clients = load_data('clients.json')
+    data_payments = load_data('payments.json')
+    data = {'clients': data_clients['clients'], 'payments': data_payments['payments']}
+    timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    output_file = f'Task1{timestamp}.xlsx'
+    xls_writer = XlsAnalyticPaymentWriter(data)
+    xls_writer.write_excel_report(output_file)
+    print(f"Невероятно, ОНО РАБОТАЕТ!♥ Моя цацка: {output_file}")
