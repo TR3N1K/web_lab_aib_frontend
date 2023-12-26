3983d9f65e770cc6d511c5f46444a1a80de109a6
TR3N1K <DEEESg@yandex.ru>
2023-12-26 11:53:22 +0300

    Update

--- labs/Lab_12_python_intro/Solution/Task2/Task_2New.py
+++ labs/Lab_12_python_intro/Solution/Task2/Task_2New.py
@@ -1,24 +1,16 @@
-def find_median_sum(n, X):
-    medians = []  # Список для хранения медиан
+import heapq
 
-    for i in range(1, n + 1):
-        subsequence = X[:i]  # Первые i элементов последовательности X
-        subsequence.sort()  # Сортировка подпоследовательности
+def MedianSearch(n, arr):
+    max = [], min = [], result = 0
+    for i in range(n):
+        heapq.heappush(max, -arr[i])  
+        heapq.heappush(min, -heapq.heappop(max))
+        
+        if len(min) > len(max):
+            heapq.heappush(max, -heapq.heappop(min))  
+        result -= max[0]  
 
-        length = len(subsequence)
-        if length % 2 == 1:  # Нечетная длина
-            median = subsequence[length // 2]
-        else:  # Четная длина
-            median = (subsequence[length // 2 - 1] + subsequence[length // 2]) / 2
+    return result
 
-        medians.append(median)  # Добавление найденной медианы в список
-
-    median_sum = sum(medians)  # Сумма всех найденных медиан
-    return median_sum
-
-# Пример использования
-n = int(input("Введите n: "))
-X = list(map(int, input("Введите последовательность чисел X: ").split()))
-
-result = find_median_sum(n, X)
-print(result)
+n, arr =int(input()), list(map(int, input().split()))
+print(MedianSearch(n, arr))
